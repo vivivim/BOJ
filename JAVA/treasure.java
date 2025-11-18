200b6076c1d7696bd05f24f1490e533c621e4b03
성현주 <130360809+vivivim@users.noreply.github.com>
2025-11-18 13:56:42 +0900

    boj: 보물 쉽다 ㅋ 프로그래머스랑 따로 폴더 정리함

---
+++ JAVA/treasure.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class treasure {
+    static int n = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        int[] a = new int[n];
+        int[] b = new int[n];
+
+        for (int i = 0; i < n; ++i)
+            a[i] = sc.nextInt();
+        for (int i = 0; i < n; ++i)
+            b[i] = sc.nextInt();
+
+        int answer = 0;
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        for (int i = 0; i < n; ++i)
+            answer += a[i] * b[n-i-1];
+
+        System.out.println(answer);
+        sc.close();
+    }
+}
