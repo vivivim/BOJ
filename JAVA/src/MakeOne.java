2606e24d6cf7c2fbba769e66cbf994fdbdf8dd91
성현주 <130360809+vivivim@users.noreply.github.com>
2025-12-08 17:54:19 +0900

    boj: 1로 만들기 디피 .. 조건을 잘 봐야지ㅑ

---
+++ JAVA/src/MakeOne.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MakeOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] dp = new int[n+1];
+
+        dp[1] = 0;
+
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = dp[i-1] + 1;
+
+            if (i % 3 == 0)
+                dp[i] = Math.min(dp[i/3] + 1, dp[i]);
+            if (i % 2 == 0)
+                dp[i] = Math.min(dp[i/2] + 1, dp[i]);
+        }
+
+        System.out.println(dp[n]);
+    }
+}
