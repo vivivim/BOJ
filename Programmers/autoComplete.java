f188ae0dcd2ebc33c4e3fa7a437d508dd48703be
성현주 <130360809+vivivim@users.noreply.github.com>
2025-12-08 16:59:29 +0900

    Prog: 자동 완성 트라이라는 구조를 새롭게 알게 됐다. 트리인데 문자열 검색에 최적이라고 하네요. 적당히 어렵고 좋네요.

---
+++ Programmers/autoComplete.java
@@ -0,0 +1,54 @@
+class Solution {
+    class Node {
+        Node[] child = new Node[26];
+        int preCt;
+            
+        Node(int preCt) {
+            this.preCt = preCt;
+        }
+    }
+    
+    public int solution(String[] words) {
+        int answer = 0;
+        Node root = new Node(0);
+        
+        for (String word : words) {
+            int n = word.length();
+            Node cur = root;
+            
+            for (int i = 0; i < n; ++i) {
+                char c = word.charAt(i);
+                
+                if (cur.child[c - 'a'] == null) {
+                    cur.child[c - 'a'] = i == n-1 ? new Node(1) : new Node(1);
+                    cur = cur.child[c - 'a'];
+                } else {
+                    cur = cur.child[c - 'a'];
+                    ++cur.preCt;
+                }
+            }
+        }
+        
+        for (String word : words) {
+            int n = word.length();
+            int ct = 0;
+            Node cur = root;
+            
+            for (int i = 0; i < n; ++i) {
+                char c = word.charAt(i);
+                cur = cur.child[c - 'a'];
+                
+                if (cur.preCt == 1) {
+                    ++ct;
+                    break ;
+                } else
+                    ++ct;
+            }
+            
+            answer += ct;
+        }
+        
+        
+        return answer;
+    }
+}
