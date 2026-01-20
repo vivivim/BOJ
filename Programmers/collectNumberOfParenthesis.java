d4735f3aba2844c205fe898ce82a62333d11127a
성현주 <130360809+vivivim@users.noreply.github.com>
2026-01-20 23:59:02 +0900

    Prog: 올바른 괄호의 갯수 레벨 4치고는 무난 ..? 아닌가 .. 디피

---
+++ Programmers/collectNumberOfParenthesis.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] dp = new int[15];
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= 14; ++i) {
+            for (int j = 0; j < i; ++j) {
+                dp[i] += dp[j] * dp[i-1 - j];
+            }
+        }
+        
+        return dp[n];
+    }
+}
